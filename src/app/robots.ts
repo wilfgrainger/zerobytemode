4e8658372414c87ec866067327f0a063ed9a40b6
wilfgrainger <51911955+wilfgrainger@users.noreply.github.com>
2026-07-31 11:23:37 +0100

    Use the canonical root domain in robots metadata

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,13 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
 export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/verify/'],
+      userAgent: "*",
+      allow: "/",
     },
-    sitemap: 'https://www.zerobytemode.com/sitemap.xml',
+    sitemap: "https://zerobytemode.com/sitemap.xml",
   };
 }
